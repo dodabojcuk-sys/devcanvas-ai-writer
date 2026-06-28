35374199ce5c116dc4ce498ef9d3e017d5b0ad64
dodabojcuk-sys <dodabojcuk@gmail.com>
2026-06-28 20:38:48 +0800

    task-035: add NarrativeSnapshot entry

---
+++ runtime/execution/narrative/NarrativeSnapshotAssembler.ts
@@ -0,0 +1,9 @@
+export {
+  assembleNarrativeGraphSnapshot,
+  assembleNarrativeGraphSnapshot as assembleNarrativeSnapshot,
+} from "../../narrative/narrativeGraphSnapshotAssembler";
+
+export type {
+  NarrativeGraphSnapshotAssemblerInput,
+  NarrativeGraphSnapshotAssemblerInput as NarrativeSnapshotAssemblerInput,
+} from "../../narrative/narrativeGraphSnapshotAssembler";
